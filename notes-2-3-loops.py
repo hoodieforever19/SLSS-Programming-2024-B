2e987b9610b371d464527acf4f022ed33068b609
Derek Nong <dereknong19@gmail.com>
2024-04-05 10:48:52 -0700

    added for and while loops

--- notes-2-3-loops.py
+++ notes-2-3-loops.py
@@ -1,3 +1,49 @@
 #loops and iteration
 #derek Nong
 #April 9 2024
+
+#print  "something 10 times"
+for _ in range(10):
+    print("something 10 times")
+
+#print out every item in my grocery list
+
+grocery_list = [
+    "dishwasher tabs",
+    "aluminium foil",
+    "blueberry muffins",
+    "RTX 4070 Super",
+]
+
+#stop when we reach blueberry muffins
+for item in grocery_list:
+    print("-------")
+    print(f" * {item}")
+
+
+    if item == "blueberry muffins":
+        break 
+
+
+#count from 0-9
+for i in range(10) :
+    print(i)
+
+    # modulo, or what is the remainder of a diviision
+    if i%2 == 0:
+        print(f"{i} is an even number")
+
+
+#rewrite the above loop as a while loop
+counter = 0
+
+while counter < 1000:
+    if counter % 2 == 0:
+        print(f"{counter} is an even number")
+    else:
+        print(counter)
+
+
+    #increment counter by 1
+    #counter = counter +1
+    counter += 1
