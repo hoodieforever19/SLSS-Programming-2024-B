8f8ab6266543c238ed1b2d203aa04ef191db64b1
hoodieforever19 <163197245+hoodieforever19@users.noreply.github.com>
2024-06-20 19:19:01 -0700

    added for submission

---
+++ unit-mid-2-activity.py
@@ -0,0 +1,28 @@
+#Unit mid 2 activity
+#Derek Nong
+#June 19th 2024
+
+def isfruitstock(fruit, stock):
+    """
+    Check if a fruit is in stock
+    Params:
+        fruit: The name of the fruit to check
+        stock: A list of fruits that are in stock
+    Returns:
+        True if the fruit is in stock, False otherwise
+    """
+    return fruit in stock
+
+# List of fruits in stock
+fruits_in_stock = ["apple", "orange", "blueberry", "grape", "kiwi"]
+
+# List of fruits to check
+fruits_to_check = ["apple", "watermelon", "kiwi", "dragonfruit"]
+
+# Check availability of each fruit
+for fruit in fruits_to_check:
+    in_stock = isfruitstock(fruit, fruits_in_stock)
+    if in_stock:
+        print(f"The fruit {fruit} is in stock.")
+    else:
+        print(f"The fruit {fruit} is not in stock.")
